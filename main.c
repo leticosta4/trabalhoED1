61bdff9294d79d9b88f4a801b4915a78bb6a5b99
leticosta4 <136915855+leticosta4@users.noreply.github.com>
2023-11-13 19:11:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,21 @@
-//#include "arvore.h"
 #include "clientes.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 void lerArquivoArvore(FILE *arquivo, int qtd, Arvore **viagem);
-void lerArquivoLista(FILE *arquivo, Pais **pais, Sitio **sitio);
+void lerArquivoLista(FILE *arquivo, Pais **paises, Sitio **sitio);
 
 int main() {
   FILE *arquivo = NULL;
   Arvore *viagem = NULL;
-  Pais *pais = NULL;
-  Sitio *sitio = NULL; //isso aq da p tirar
+  Pais *paises = NULL;
+  Sitio *sitio = NULL; //nao tirar por causa da chamada da função lerArquivoLista
   int qtd = 0;
 
   lerArquivoArvore(arquivo, qtd, &viagem);
-  lerArquivoLista(arquivo, &pais, &sitio);
+  lerArquivoLista(arquivo, &paises, &sitio);
   listarArvore(viagem);
-  listarLista(pais);
+  listarLista(paises);
 
 
   return 0;
@@ -41,7 +40,7 @@ void lerArquivoArvore(FILE *arquivo, int qtd, Arvore **viagem) {
   fclose(arquivo);
 }
 
-void lerArquivoLista(FILE *arquivo, Pais **pais, Sitio **sitio) {
+void lerArquivoLista(FILE *arquivo, Pais **paises, Sitio **sitio) {
   char nome[50];
   int indice = 0;
 
@@ -53,9 +52,9 @@ void lerArquivoLista(FILE *arquivo, Pais **pais, Sitio **sitio) {
   for (int i = 0; i < 26; i++) {
     fscanf(arquivo, "%d, %[^\n]", &indice, nome);
     if (indice == 0) {
-      (*pais) = inserirPaises(*pais, nome);
+      (*paises) = inserirPaises(*paises, nome);
     } else {
-      (*pais)->local = inserirCidade((*pais)->local, nome);
+      (*paises)->local = inserirCidade((*paises)->local, nome);
     }
   }
   fclose(arquivo);
