ce35df196472d26910fdac1be52b89c9e8fab962
Cainan <137329310+Cainan-bas@users.noreply.github.com>
2023-11-22 22:20:22 -0300

    Create cores.h

---
+++ cores.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+
+void roxo(){
+  printf("\033[0;35m");
+}
+
+void azul(){
+  printf("\033[0;34m");
+}
+
+void branco(){
+  printf("\033[0;37m");
+}
+
+void vermelho(){
+  printf("\033[0;31m");
+}
